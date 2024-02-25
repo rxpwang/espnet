d6def8aa08250486e5df8f36636930b64159e086
Rongxiang Wang <wrxwst@gmail.com>
2024-02-25 14:29:36 -0500

    Xavier configuration

--- egs2/TEMPLATE/asr1/path.sh
+++ egs2/TEMPLATE/asr1/path.sh
@@ -9,7 +9,7 @@ export LC_ALL=C
 . "${MAIN_ROOT}"/tools/activate_python.sh && . "${MAIN_ROOT}"/tools/extra_path.sh
 export PATH=$MAIN_ROOT/utils:$MAIN_ROOT/espnet/bin:$PATH
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=8
 
 # NOTE(kan-bayashi): Use UTF-8 in Python to avoid UnicodeDecodeError when LC_ALL=C
 export PYTHONIOENCODING=UTF-8
